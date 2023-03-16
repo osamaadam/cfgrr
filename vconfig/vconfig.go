6b575683089aedf0567c5d0b2af99525d62a155c
Osama Adam <osamaadamme@gmail.com>
2023-03-16 16:20:00 +0200

    fixing history

--- vconfig/vconfig.go
+++ vconfig/vconfig.go
@@ -167,7 +167,7 @@ func (c *Config) init() error {
 			c.SetBackupDir(defaultConfigDir)
 			c.SetMapFile("cfgrrmap.yaml")
 			c.SetIgnoreFile(".cfgrrignore")
-			if err := viper.SafeWriteConfig(); err != nil {
+			if err := v.SafeWriteConfig(); err != nil {
 				return errors.WithStack(err)
 			}
 		} else {
